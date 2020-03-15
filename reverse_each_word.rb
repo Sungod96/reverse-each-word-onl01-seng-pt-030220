557de49a5d6b05cb74be2b1beac339fea0578b86
Amon-Ra Sanders <mrlive2k@gmail.com>
2020-03-15 21:22:58 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,28 @@
+require 'pry'
+
+string =  "Hello there, how are you?"
+    
+def reverse_each_word(string)
+      new_array = []
+      string.split.each do |word|
+    end
+    
+    
+    
+    
+      
+      
+     
+      
+     end
+     
+      
+      
+      
+      
+  
+  
+
+
+
+
